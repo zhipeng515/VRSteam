e852e4ea6721bab1a348d5d2772b992081535dd4
zhipeng <zhangzhipeng@973.sinanet.com>
2016-07-15 13:24:38 +0800

    使用自定义TitleBar替换系统默认的

---
+++ menubar.cpp
@@ -0,0 +1,6 @@
+#include "menubar.h"
+
+MenuBar::MenuBar()
+{
+
+}
---
+++ menubar.h
@@ -0,0 +1,11 @@
+#ifndef MENUBAR_H
+#define MENUBAR_H
+
+
+class MenuBar : public QMenuBar
+{
+public:
+    MenuBar();
+};
+
+#endif // MENUBAR_H
